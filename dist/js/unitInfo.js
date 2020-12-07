56369d07fcce37875003a9eee38a50bee586974e
ZEMontreuil <zemontreuil@DESKTOP-06JIMO5.localdomain>
2020-12-07 15:13:34 -0600

    Adds concatenation, minification, souremaps to gulp JS tasks. Adds html and CSS tasks that move them to dist directory.

--- dist/js/unitInfo.js
+++
@@ -1,8 +0,0 @@
-class Unit {
-  constructor(name, role, power, models) {
-    this.name = name;
-    this.role = role;
-    this.power = power;
-    this.models = models;
-  }
-}
--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,25 @@
 const GulpClient = require('gulp');
-const {src, dest, series} = require('gulp');
+const {src, dest, series, parallel} = require('gulp');
 const minify = require('gulp-minify');
+const concat = require('gulp-concat');
 
 const scriptTask = () => {
-  return src('src/**/*.js')
+  return src('src/**/*.js', {sourcemaps: true})
+    .pipe(minify({
+      noSource: true,
+    }))
+    .pipe(concat("all.js"))
+    .pipe(dest('dist/js/', {sourcemaps: true}));
+}
+
+const markupTask = () => {
+  return src('src/*.html')
     .pipe(dest('dist/'));
 }
 
-exports.scripts = series(scriptTask);
+const styleTask = () => {
+  return src('src/*.css')
+    .pipe(dest('dist/'));
+}
+
+exports.all = parallel(scriptTask, styleTask, markupTask);
